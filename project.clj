01fa2e981ebe7d11c60a8dd26ce107b9b7538493
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2014-01-05 22:25:58 +0100

    First shot at download and unzip.
    Downloading as a stream directly to a file, then unzipping and
    removing zip file.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.5"]
-                 [clj-http "0.7.8"]]
+                 [clj-http "0.7.8"]
+                 [me.raynes/fs "1.4.4"]]
   :main ^:skip-aot datomic-free.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/datomic_free/core.clj
+++ src/datomic_free/core.clj
@@ -1,16 +1,44 @@
 (ns datomic-free.core
-  (:require [net.cgrand.enlive-html :as html])
+  (:require [net.cgrand.enlive-html :as html]
+            [me.raynes.fs :as fs]
+            [me.raynes.fs.compression :refer [unzip]]
+            [clj-http.client :as client]
+            [clojure.java.io :refer [copy file]])
   (:gen-class))
+
+(def ^:const HOME (System/getenv "HOME"))
+(def ^:const DATOMIC_FREE_HOME (str HOME "/.datomic-free"))
+(def ^:const DATOMIC_FREE_VERSIONS (str DATOMIC_FREE_HOME "/versions"))
+(def ^:const DATOMIC_FREE_ACTIVE (str DATOMIC_FREE_HOME "/active"))
+(def ^:const DATOMIC_FREE_DATA (str DATOMIC_FREE_HOME "/data"))
 
 (def ^:dynamic *base-url* "https://my.datomic.com")
 
 (defn fetch-url []
   (html/html-resource (java.net.URI. (str *base-url* "/downloads/free"))))
 
-(defn latest [node]
-  (-> node (html/select [:a.latest]) first :attrs :href))
+(defn get-latest-datomic-version []
+  (str *base-url* (-> (fetch-url) (html/select [:a.latest]) first :attrs :href)))
+
+(defn download-datomic [version]
+  (let [path (str DATOMIC_FREE_VERSIONS "/datomic-free-" version)]
+    (if (fs/exists? path)
+      (println (format "Datomic Free %s is already present." version))
+      (do
+        (fs/mkdirs DATOMIC_FREE_VERSIONS)
+        (let [url (str *base-url* "/downloads/free/" version)
+              response (client/get url {:as :stream})
+              zip-file "datomic-free.zip"]
+          (if (= 200 (:status response))
+            (do
+              (with-open [i (:body response)]
+                (copy i (file (str DATOMIC_FREE_VERSIONS "/" zip-file))))
+              (fs/with-cwd DATOMIC_FREE_VERSIONS
+                (unzip zip-file ".")
+                (fs/delete zip-file)))
+            (println version "is not a valid version. See https://my.datomic.com/downloads/free for a list of versions.")))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (str *base-url* (latest (fetch-url)))))
+  (println (get-latest-datomic-version)))

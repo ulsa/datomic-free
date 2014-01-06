c4a97e7a54fc16a050650a76a93dcc72bbe131d6
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2014-01-06 21:28:22 +0100

    Now installing maven artifacts.

--- src/datomic_free/core.clj
+++ src/datomic_free/core.clj
@@ -6,8 +6,10 @@
             [clojure.java.io :refer [copy file]]
             [clojure.string :as string]
             [clojure.java.shell :refer [sh]]
-            [clojure.tools.cli :refer [parse-opts]])
-  (:import [java.nio.file Files Paths Path]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.stacktrace :refer [print-throwable]])
+  (:import java.io.IOException
+           [java.nio.file Files Paths Path]
            [java.nio.file.attribute FileAttribute])
   (:gen-class))
 
@@ -18,38 +20,86 @@
 (def ^:dynamic *active-path* (str *home-path* "/active"))
 (def ^:dynamic *data-path* (str *home-path* "/data"))
 
-(defn fetch-url []
+(defn- fetch-url []
   (html/html-resource (java.net.URI. (str *base-url* "/downloads/free"))))
 
 (defn get-latest-datomic-version []
   (-> (fetch-url) (html/select [:a.latest]) first :attrs :href (string/split #"/") last))
 
-(defn path-to-version [version]
+(defn path-for-version [version]
   (str *versions-path* "/datomic-free-" version))
 
-(defn make-transactor-executable [version]
+(defn- to-path [p]
+  (if (instance? Path p)
+    p
+    (Paths/get p (into-array String []))))
+
+(defn- symlink [link target]
+  (Files/createSymbolicLink (to-path link)
+                            (to-path target)
+                            (into-array FileAttribute [])))
+
+(defn- symlink-target [link]
+  (-> link (to-path) (Files/readSymbolicLink)))
+
+(defn- copy-stream-to-file [stream zip-file]
+  (with-open [i stream]
+    (copy i (file (str *versions-path* "/" zip-file)))))
+
+(defn- unzip-and-delete [zip-file]
+  (fs/with-cwd *versions-path*
+    (unzip zip-file ".")
+    (fs/delete zip-file)))
+
+;; TODO make everything in bin executable recursively, using fs/walk and fs/chmod
+(defn- make-transactor-executable [version]
   (println "Making datomic executable")
-  (doseq [executable ["bin/transactor" "bin/classpath" "bin/maven-install"]]
-    (fs/chmod "+x" (str (path-to-version version) "/" executable))))
+  (doseq [executable ["bin/transactor" "bin/classpath" "bin/maven-install"]
+          :let [path (str (path-for-version version) "/" executable)]]
+    (when (fs/exists? path)
+      (fs/chmod "+x" path))))
+
+(defn- install-maven-artifacts [version]
+  (try
+    (sh "mvn" "-v")
+    (println (format "Installing datomic-free-%s in local maven repository..." version))
+    (try
+      (let [p (sh "bin/maven-install" :dir (str *versions-path* "/" version))
+            status (:exit p)]
+        (when-not (zero? status)
+          (println (:err p))))
+      (catch Exception e
+        (println "Problem running Datomic script 'bin/maven-install':") (print-throwable e)))
+    (catch IOException e
+      (println "No 'mvn' command available, skipping install in local maven repository"))))
+
+(defn use-datomic [version]
+  (fs/delete *active-path*)
+  (symlink *active-path* (str *versions-path* "/datomic-free-" version)))
+
+(defn update-data-dir [version]
+  ;; TODO do stuff here
+  )
 
 (defn download-datomic [version]
-  (let [path (path-to-version version)]
+  (let [path (path-for-version version)]
     (if (fs/exists? path)
       (println (format "Datomic Free %s is already present." version))
       (do
         (fs/mkdirs *versions-path*)
-        (printf "Downloading Datomic Free %s...\n" version)
+        (println (format "Downloading Datomic Free %s.." version))
         (let [url (str *base-url* "/downloads/free/" version)
               response (client/get url {:as :stream})
               zip-file "datomic-free.zip"]
           (if (= 200 (:status response))
             (do
-              (with-open [i (:body response)]
-                (copy i (file (str *versions-path* "/" zip-file))))
-              (fs/with-cwd *versions-path*
-                (unzip zip-file ".")
-                (fs/delete zip-file))
-              (make-transactor-executable version))
+              (copy-stream-to-file (:body response) zip-file)
+              (unzip-and-delete zip-file)
+              (make-transactor-executable version)
+              (install-maven-artifacts version)
+              (use-datomic version)
+              (update-data-dir version)
+              (println (format "Done. Datomic Free %s is now available." version)))
             (println version "is not a valid version. See https://my.datomic.com/downloads/free for a list of versions.")))))))
 
 (defn download-latest-datomic []
@@ -58,23 +108,6 @@
     (if (string/blank? version)
       (println "The latest version could not be found. Install a specific version with \"datomic-free update VERSION\".")
       (download-datomic version))))
-
-(defn to-path [p]
-  (if (instance? Path p)
-    p
-    (Paths/get p (into-array String []))))
-
-(defn symlink [link target]
-  (Files/createSymbolicLink (to-path link)
-                            (to-path target)
-                            (into-array FileAttribute [])))
-
-(defn symlink-target [link]
-  (-> link (to-path) (Files/readSymbolicLink)))
-
-(defn use-datomic [version]
-  (fs/delete *active-path*)
-  (symlink *active-path* (str *versions-path* "/datomic-free-" version)))
 
 (defn start-transactor
   ([] (start-transactor (str *active-path* "/config/samples/free-transactor-template.properties")))
